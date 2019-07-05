357e0b9978c8cbbcb052be79cffced94386ffa28
Atanas Chuchulev <achuchulev@Atanass-MBP.lan>
2019-07-05 11:37:34 +0300

    add tag name output

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "vpc_id" {
   value = "${aws_vpc.new_vpc.id}"
 }
 
+output "vpc_name" {
+  value = "${aws_vpc.new_vpc.tags.Name}"
+}
+
 output "subnet_ids" {
   value = "${aws_subnet.vpc_subnet.*.id}"
 }
