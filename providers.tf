aee255751d9ae0ed2cc85dce85105f8358b63569
Atanas Chuchulev <atanas.v4@gmail.com>
2022-01-24 14:30:27 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.aws_region
 
   # Requester's credentials.
-  #access_key = var.aws_access_key
-  #secret_key = var.aws_secret_key
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
